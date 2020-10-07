c40250c34cba6e322375e54e656a44ed25095c76
Sille Van Landschoot <info@sillevl.be>
2020-10-07 10:17:11 +0200

    our first working webserver

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("hello world")
+// const net = require('net');
+import net from 'net'
+
+const server = net.createServer( (client) => {
+    console.log('client connected')
+    client.write('http/1.1 200 OK\r\n\r\n<h1>Yooo</h1>')
+    client.end()
+})
+
+server.listen(80, () => {
+    console.log('welcome to port 80')
+})
+
+console.log('*** HTTP Server started ***')
